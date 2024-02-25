74f1137bf32e197498692ea8a93596ce684928a0
CamiloLoaiza12 <camilo.loaizag@udea.edu.co>
2024-02-25 16:07:45 -0500

    Algunos graficos

--- c_Exploracion_de_datos.py
+++ c_Exploracion_de_datos.py
@@ -15,6 +15,38 @@ tabla=pd.read_sql("""select *
                      from tabla_completa """ , con)
 
 ### explorar variable respuesta ###
-fig=tabla.v_objetivo.hist(bins=50,ec='black') ## no hay atípicos
+fig=tabla.v_objetivo.hist(bins=20,ec='black') ## no hay atípicos
 fig.grid(False)
 plt.show()
+
+boxprops = dict(linestyle='-', color='black')
+medianprops = dict(linestyle='-',  color='black')
+fig=tabla.boxplot("v_objetivo",patch_artist=True,
+                boxprops=boxprops,
+                medianprops=medianprops,
+                whiskerprops=dict(color='black'),
+                showmeans=True)
+fig.grid(False)
+plt.show()
+
+####explorar variables numéricas con histograma
+fig=tabla.hist(bins=50, figsize=(40,30),grid=False,ec='black')
+plt.show()
+
+continuas = ['v_objetivo',
+             'EnvironmentSatisfaction',
+             'YearsAtCompany',
+             'WorkLifeBalance',
+             'JobInvolvement',
+             ]
+scatter_matrix(tabla[continuas], figsize=(12, 8))
+plt.show()
+cont=tabla[continuas]
+corr_matrix = cont.corr()
+corr_matrix["v_objetivo"].sort_values(ascending=False)
+
+
+##### analizar relación con categóricas ####
+
+tabla.boxplot("v_objetivo","JobRole",figsize=(5,5),grid=False)
+tabla.boxplot("v_objetivo","BusinessTravel",figsize=(5,5),grid=False)
