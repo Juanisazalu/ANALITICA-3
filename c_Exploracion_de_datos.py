78cbd8585466d5dca370b70a560e4ea43a942235
CamiloLoaiza12 <camilo.loaizag@udea.edu.co>
2024-02-26 13:11:54 -0500

    Se agrega distribución variables númericas (grafico)

--- c_Exploracion_de_datos.py
+++ c_Exploracion_de_datos.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt ### gráficos
 from pandas.plotting import scatter_matrix  ## para matriz de correlaciones
 from sklearn import tree ###para ajustar arboles de decisión
 from sklearn.tree import export_text ## para exportar reglas del árbol
+import seaborn as sns
 
 ## Visualization
 import matplotlib.pyplot as plt
@@ -39,6 +40,13 @@ fig=tabla.boxplot("v_objetivo",patch_artist=True,
 fig.grid(False)
 plt.show()"""
 
+### Dimensiones del dataset
+tabla.shape
+
+# Número de datos ausentes por variable
+tabla.isna().sum().sort_values()
+
+
 tabla.describe()
 
 tabla.info()
@@ -54,15 +62,36 @@ cont=tabla[continuas]
 corr_matrix = cont.corr()
 
 
+### Gráfico de distribución de las variables númericas
+fig, axes = plt.subplots(nrows=4, ncols=4, figsize=(9, 5))
+axes = axes.flat
+columnas_numeric = tabla.select_dtypes(include=['float64', 'int']).columns
+columnas_numeric = columnas_numeric.drop('v_objetivo')
+
+for i, colum in enumerate(columnas_numeric):
+    sns.histplot(
+        data     = tabla,
+        x        = colum,
+        stat     = "count",
+        kde      = True,
+        color    = (list(plt.rcParams['axes.prop_cycle'])*2)[i]["color"],
+        line_kws = {'linewidth': 2},
+        alpha    = 0.3,
+        ax       = axes[i]
+    )
+    axes[i].set_title(colum, fontsize = 7, fontweight = "bold")
+    axes[i].tick_params(labelsize = 6)
+    axes[i].set_xlabel("")
+    
+    
+fig.tight_layout()
+plt.subplots_adjust(top = 0.9)
+fig.suptitle('Distribución variables numéricas', fontsize = 10, fontweight = "bold")
 
 
-##### analizar relación con categóricas ####
+##### Variable categóricas ####
 
-tabla.boxplot("v_objetivo","jobrole",figsize=(5,5),grid=False)
-tabla.barplot("v_objetivo","businesstravel",figsize=(5,5),grid=False)
-
-
-plt.figure(figsize=(20, 10))
+plt.figure(figsize=(25, 10))
 plt.subplot(2,3,1)
 tabla['businesstravel'].value_counts().plot(kind='pie',autopct='%.2f')
 plt.subplot(2,3,2)
@@ -73,5 +102,12 @@ plt.subplot(2,3,4)
 tabla['educationfield'].value_counts().plot(kind='pie',autopct='%.2f')
 plt.subplot(2,3,5)
 tabla['maritalstatus'].value_counts().plot(kind='pie',autopct='%.2f')
+
+### relación con variable objetivo
+
+tabla.boxplot("v_objetivo","jobrole",figsize=(5,5),grid=False)
+tabla.barplot("v_objetivo","businesstravel",figsize=(5,5),grid=False)
+
+sns.barplot(x = "v_objetivo", y = "businesstravel", data = tabla)
 
 
