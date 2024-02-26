0ce1059ee02f4caa4a130fa6f593ec684f67fd43
juanisazalu <143090388+Juanisazalu@users.noreply.github.com>
2024-02-26 10:54:30 -0500

    Falta organizar exploracion

--- b_preprocesamiento.py
+++ b_preprocesamiento.py
@@ -1,3 +1,4 @@
+#Importación de librerias
 import pandas as pd
 import sqlite3 as sql #### para bases de datos sql
 import a_funciones as funciones
@@ -7,43 +8,58 @@ from matplotlib.pyplot import figure
 import seaborn as sns
 from itertools import product
 
+#Ruta 
 sys.path
 #sys.path.append('c:\\cod\\LEA3_HR\\data') ## este comanda agrega una ruta
 
+#Lectura de archivos
 encuesta_empleado=pd.read_csv("data\employee_survey_data.csv")
 general_data=pd.read_csv("data\general_data.csv")
 encuesta_gerente=pd.read_csv("data\manager_survey.csv")
 info_retiros=pd.read_csv("data/retirement_info.csv")
 
-encuesta_empleado
-general_data
-encuesta_gerente
-info_retiros
+#Vista general
+encuesta_empleado.head()
+general_data.head()
+encuesta_gerente.head()
+info_retiros.head()
 
-#.info()
+#Información
 encuesta_empleado.info() #describir que variables tienen nulos
 general_data.info()
 encuesta_gerente.info() #no tiene nulos
 info_retiros.info()
 
-#los codigos de acciones se pasan a object generalmente
 #Cambio de formato de fecha
 encuesta_empleado["DateSurvey"]
 encuesta_empleado["DateSurvey"] = pd.to_datetime(encuesta_empleado["DateSurvey"])
 
 general_data["InfoDate"]
 general_data["InfoDate"]=pd.to_datetime(general_data["InfoDate"])
-general_data["EmployeeCount"].value_counts()
-general_data["Over18"].value_counts()
-#justificar
-#variables a eliminar de entrada:EmployeeCount, Over18                               
+                              
 encuesta_gerente["SurveyDate"]
 encuesta_gerente["SurveyDate"]=pd.to_datetime(encuesta_gerente["SurveyDate"])
 
 info_retiros["retirementDate"]
 info_retiros["retirementDate"]=pd.to_datetime(info_retiros["retirementDate"])
 
-#Eliminacion de columnas
+#Valores unicos para las variables exploración inicial
+for col in general_data.columns:
+    funciones.cat_summary(general_data, col)
+ 
+for col in encuesta_empleado.columns:
+    funciones.cat_summary(encuesta_empleado, col)
+
+for col in encuesta_gerente.columns:
+    funciones.cat_summary(encuesta_gerente, col)
+
+for col in info_retiros.columns:
+    funciones.cat_summary(info_retiros, col)
+#La exploración anterior arroja que las variables  EmployeeCount, Over18, StandardHours
+#son constantes y por lo tanto no aportaran nada al modelo, ademas algunas otras variables
+#que no tienen un sentido claro.
+    
+#Eliminacion de columnas segun la exploración
 encuesta_empleado.drop("Unnamed: 0", axis=1, inplace=True)
 general_data.drop("Unnamed: 0", axis=1, inplace=True)
 encuesta_gerente.drop("Unnamed: 0", axis=1, inplace=True)
@@ -61,41 +77,21 @@ general_data.to_sql('general_data', con, if_exists ="replace")
 encuesta_gerente.to_sql('encuesta_gerente', con, if_exists ="replace")
 info_retiros.to_sql('info_retiros', con, if_exists ="replace")
 
+#Cursor
 cur.execute("Select name from sqlite_master where type='table'") ### consultar bases de datos
 cur.fetchall()
 
-#REALIZAR EXPLORACIONES INICIALES
-for col in general_data.columns:
-    funciones.cat_summary(general_data, col)
-    #jobrole tiene 7 categorias
-#Analizar que se puede categorizar
-for col in encuesta_empleado.columns:
-    funciones.cat_summary(encuesta_empleado, col)
-#jobsatisfactio ??? tiene niveles de 1,3, 
-
-for col in encuesta_gerente.columns:
-    funciones.cat_summary(encuesta_gerente, col)
-
-for col in info_retiros.columns:
-    funciones.cat_summary(info_retiros, col)
-
-#Hacer consultas
-pd.read_sql("""select *  
-                            from info_retiros 
-                            WHERE strftime('%Y',retirementDate) = '2015' """, con)
-
-
+#Ejecutar consulta a la base de datos
 funciones.ejecutar_sql('Preprocesamiento.sql',cur)
-
-tabla=pd.read_sql("""select *  
-                     from tabla_completa """ , con)
+tabla=pd.read_sql("""select *  from tabla_completa """ , con)
 
 tabla.info()
+
 #Relleno de nulos
+tabla.isnull().sum()
 #Con la funcion no da
 cat= [x for x in tabla.columns if tabla[x].dtypes =="O"]
 funciones.imputar_f(tabla,cat)
-tabla.isnull().sum()
 
 #Manualmente
 tabla["NumCompaniesWorked"]=tabla["NumCompaniesWorked"].apply(lambda x: x if not pd.isnull(x) else int(tabla["NumCompaniesWorked"].median()))
@@ -104,16 +100,15 @@ tabla["EnvironmentSatisfaction"]=tabla["EnvironmentSatisfaction"].apply(lambda x
 tabla["JobSatisfaction"]=tabla["JobSatisfaction"].apply(lambda x: x if not pd.isnull(x) else int(tabla["JobSatisfaction"].median()))
 tabla["WorkLifeBalance"]=tabla["WorkLifeBalance"].apply(lambda x: x if not pd.isnull(x) else int(tabla["WorkLifeBalance"].median()))
 
+tabla.isnull().sum()
+#Convertir a minusculas el nombre de las columnas
 tabla.columns=tabla.columns.str.lower()
 
 #Analisis relacion entre variables numericas
-
 figure(figsize=(20,6))
 sns.heatmap(tabla.corr(),cmap = sns.cubehelix_palette(as_cmap=True), annot = True, fmt = ".2f")
 
-
-
-#Se elimina
+#Segun la matriz de correlación las variables que tienen mayor correlación con otras variables son:
 #yearsatcompany
 #totalworkingyears
 #performancerating
@@ -121,6 +116,7 @@ tabla.drop("yearsatcompany", axis=1, inplace=True)
 tabla.drop("totalworkingyears", axis=1, inplace=True)
 tabla.drop("performancerating", axis=1, inplace=True)
 
+#Resultado final
 figure(figsize=(20,6))
 sns.heatmap(tabla.corr(),cmap = sns.cubehelix_palette(as_cmap=True), annot = True, fmt = ".2f")
 
@@ -148,5 +144,5 @@ for tupla in resultados_filtrados:
 #Se elimina 
 #Genero
 tabla.drop("gender", axis=1, inplace=True)
-
+tabla.to_csv('tabla_exploración1.csv', index=False)
 #Seleccion de modelos
--- c_Exploracion_de_datos.py
+++ c_Exploracion_de_datos.py
@@ -8,18 +8,28 @@ from pandas.plotting import scatter_matrix  ## para matriz de correlaciones
 from sklearn import tree ###para ajustar arboles de decisión
 from sklearn.tree import export_text ## para exportar reglas del árbol
 
-con=sql.connect("data\\db_basedatos")
-cur=con.cursor()
+## Visualization
+import matplotlib.pyplot as plt
+import seaborn as sns
+from plotly.subplots import make_subplots
+import plotly.express as px
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
 
-tabla=pd.read_sql("""select *  
-                     from tabla_completa """ , con)
 
+tabla=pd.read_csv("tabla_exploración1.csv")
+tabla.columns
+
+cat= [x for x in tabla.columns if tabla[x].dtypes =="O"]
+continuas= tabla.select_dtypes(exclude='object')
+continuas.columns
 ### explorar variable respuesta ###
+# crear dataset
 fig=tabla.v_objetivo.hist(bins=20,ec='black') ## no hay atípicos
 fig.grid(False)
 plt.show()
 
-boxprops = dict(linestyle='-', color='black')
+"""boxprops = dict(linestyle='-', color='black')
 medianprops = dict(linestyle='-',  color='black')
 fig=tabla.boxplot("v_objetivo",patch_artist=True,
                 boxprops=boxprops,
@@ -27,20 +37,15 @@ fig=tabla.boxplot("v_objetivo",patch_artist=True,
                 whiskerprops=dict(color='black'),
                 showmeans=True)
 fig.grid(False)
-plt.show()
+plt.show()"""
 
-####explorar variables numéricas con histograma
+#explorar variables numéricas con histograma
 fig=tabla.hist(bins=50, figsize=(40,30),grid=False,ec='black')
+plt.show() 
+
+scatter_matrix(tabla[continuas.columns], figsize=(12, 8))
 plt.show()
 
-continuas = ['v_objetivo',
-             'EnvironmentSatisfaction',
-             'YearsAtCompany',
-             'WorkLifeBalance',
-             'JobInvolvement',
-             ]
-scatter_matrix(tabla[continuas], figsize=(12, 8))
-plt.show()
 cont=tabla[continuas]
 corr_matrix = cont.corr()
 corr_matrix["v_objetivo"].sort_values(ascending=False)
